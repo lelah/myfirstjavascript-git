797701e905ab62f717c54299c64371de39cddea7
lelah <lelahalh2003@gmail.com>
2019-01-11 09:10:15 -0700

    did some stuff

---
+++ myScript.js
@@ -0,0 +1,23 @@
+//This makes he button in my index work
+
+function myFunction () {
+document.getElementById('demo').innerHTML = "Paragraph Changed";
+}
+
+//Defining variables
+
+//var x = 5;
+//var y = 6;
+//var z = x + y;
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+//This function helps determine what the variables equal.
+
+function mathFunction () {
+ document.getElementById('math').innerHTML = 
+ z;
+}
